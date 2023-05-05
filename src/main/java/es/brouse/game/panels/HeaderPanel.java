e700909c8a1d3d346566d152123828d9d9ce8b5b
Brouse_13 <unaiunai2004@gmail.com>
2023-05-05 16:30:45 +0200

    [NEW] Updated HeaderPanel

--- src/main/java/es/brouse/game/panels/HeaderPanel.java
+++ src/main/java/es/brouse/game/panels/HeaderPanel.java
@@ -1,15 +1,24 @@
 package es.brouse.game.panels;
 
+import es.brouse.game.Game;
 import es.brouse.game.listeners.GameListeners;
+import es.brouse.game.listeners.ToolTipListeners;
+import es.brouse.game.objects.ButtonObject;
 import es.brouse.game.objects.SplitObject;
+import es.brouse.game.objects.ToolBarObject;
 import es.brouse.game.objects.menu.MenuHeaderObject;
 import es.brouse.game.objects.menu.MenuItemObject;
 import es.brouse.game.objects.menu.MenuObject;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URL;
 import java.util.HashSet;
 import java.util.List;
+import java.util.logging.Level;
 
 import static java.awt.BorderLayout.*;
 
@@ -22,8 +31,18 @@ public class HeaderPanel extends Panel {
 
     @Override
     public void initComponents(JPanel panel) {
+        final ToolTipListeners listeners = new ToolTipListeners();
+
         panel.add(mainMenu().getComponent(), NORTH);
-        panel.add(new ToolTipPanel().getComponent(), CENTER);
+
+        ToolBarObject toolBar = new ToolBarObject(ToolBarObject.HORIZONTAL);
+        toolBar.add(getButton("/assets/gui/game/newGame.jpg", listeners.newGame()).getComponent());
+        toolBar.add(getButton("/assets/gui/game/selectedHistory.jpg", listeners.score()).getComponent());
+        toolBar.add(getButton("/assets/gui/game/history.jpg", listeners.score()).getComponent());
+        toolBar.add(getButton("/assets/gui/game/changeDir.jpg", listeners.changeDir()).getComponent());
+        toolBar.add(getButton("/assets/gui/game/exit.jpg", listeners.exit()).getComponent());
+
+        panel.add(toolBar.getComponent(), CENTER);
         panel.add(new SplitObject(SplitObject.VERTICAL_SPLIT).getComponent(), SOUTH);
     }
 
@@ -39,5 +58,20 @@ public class HeaderPanel extends Panel {
                 new MenuItemObject("Salir", listeners.exit()));
 
         return new MenuObject(new HashSet<>(List.of(header)));
+    }
+
+    private ButtonObject getButton(String name, ActionListener listener) {
+        try {
+            //Try to load the resource
+            URL resource = getClass().getResource(name);
+            if (resource == null) throw new NullPointerException();
+
+            //Create the button
+            return new ButtonObject(null, new ImageIcon(ImageIO.read(resource)), listener);
+        } catch (IOException | NullPointerException e) {
+            //Default button and error handler
+            Game.logger.log(Level.WARNING, "Unable to load resource");
+            return new ButtonObject("X");
+        }
     }
 }
