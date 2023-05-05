ac1d1f07a00bf7b5775407885046b420b3438f85
Brouse_13 <unaiunai2004@gmail.com>
2023-05-05 14:27:28 +0200

    [NEW] Added default logger for errors

--- src/main/java/es/brouse/game/Game.java
+++ src/main/java/es/brouse/game/Game.java
@@ -4,8 +4,10 @@ import es.brouse.game.screen.GameScreen;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.logging.Logger;
 
 public class Game extends JFrame {
+    public static final Logger logger = Logger.getLogger(Game.class.getSimpleName());
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new GameScreen()::start);
