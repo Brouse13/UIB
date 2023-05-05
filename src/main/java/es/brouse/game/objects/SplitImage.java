8588a4e7cbaf136ac67cd04a82d811d1d5287d12
Brouse_13 <unaiunai2004@gmail.com>
2023-05-05 18:38:43 +0200

    [NEW] Added split images

---
+++ src/main/java/es/brouse/game/objects/SplitImage.java
@@ -0,0 +1,34 @@
+package es.brouse.game.objects;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Class in charge of the storage of the parts of a complete
+ * rendered image Furthermore, each part will be identified by an index
+ * to make sure order integrity.
+ */
+public class SplitImage {
+    /*---------- PRIVATE ----------*/
+    private final BufferedImage image;
+    private final int index;
+
+    /**
+     * Main class constructor able to create new {@link SplitImage}
+     * instance.
+     *
+     * @param image image bytes
+     * @param index image index
+     */
+    public SplitImage(BufferedImage image, int index) {
+        this.image = image;
+        this.index = index;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+}
---
+++ src/main/java/es/brouse/game/utils/ImageUtils.java
@@ -0,0 +1,62 @@
+package es.brouse.game.utils;
+
+import es.brouse.game.Game;
+import es.brouse.game.objects.SplitImage;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+
+public class ImageUtils {
+    public Set<SplitImage> split(String fileName, int rows, int cols) {
+        //Read the originalImage or log an exception
+        final BufferedImage originalImage;
+        try {
+            originalImage = loadImage(fileName);
+        }catch (IOException e) {
+            Game.logger.log(Level.WARNING, e.getMessage());
+            return new HashSet<>();
+        }
+
+        //Storage of all the returned subImages
+        Set<SplitImage> images = new HashSet<>(rows  * cols);
+
+        //With and height of each section
+        int width = originalImage.getWidth() / cols;
+        int height = originalImage.getHeight() / rows;
+
+        int index = 0;
+
+        // iterating over rows and columns for each sub-originalImage
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                //Creating subImage
+                BufferedImage image = new BufferedImage(width, height, originalImage.getType());
+                Graphics2D img_creator = image.createGraphics();
+
+                // coordinates of source originalImage
+                int src_x = width * j;
+                int src_y = height * i;
+                int dst_x = width * j + width;
+                int dst_y = height * i + height;
+
+                img_creator.drawImage(originalImage, 0, 0, width, height, src_x, src_y, dst_x, dst_y, null);
+                images.add(new SplitImage(image, index));
+                index++;
+            }
+        }
+        return images;
+    }
+
+    public BufferedImage loadImage(String path) throws IOException {
+        URL resource = getClass().getResource(path);
+        if (resource == null) throw new IOException("Image " + path + " not found");
+
+        return ImageIO.read(resource);
+    }
+}
