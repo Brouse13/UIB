70fe7ddae7d7049808d99edc0cd1af6427ed3200
Brouse_13 <unaiunai2004@gmail.com>
2023-05-08 17:16:39 +0200

    [FIX] Added System LookAndLike

--- src/main/java/es/brouse/game/screen/GameScreen.java
+++ src/main/java/es/brouse/game/screen/GameScreen.java
@@ -1,11 +1,13 @@
 package es.brouse.game.screen;
 
+import es.brouse.game.Game;
 import es.brouse.game.panels.GamePanel;
 import es.brouse.game.panels.Panel;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.Locale;
+import java.util.logging.Level;
 
 public class GameScreen extends Screen {
     @Override
@@ -22,6 +24,11 @@ public class GameScreen extends Screen {
     public void setUp(JFrame frame) {
         frame.setSize(new Dimension(500, 500));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            Game.logger.log(Level.WARNING, "Couldn't assign system LookAndFeel");
+        }
         frame.setLocale(Locale.ROOT);
     }
 }
