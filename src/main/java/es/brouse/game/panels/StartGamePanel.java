f66ba6cd21fba985072f17f0e6b7bef9f8b1669f
Brouse_13 <unaiunai2004@gmail.com>
2023-05-09 15:57:41 +0200

    [NEW] Created new panel and added test image

--- src/main/java/es/brouse/game/panels/StartGamePanel.java
+++ src/main/java/es/brouse/game/panels/StartGamePanel.java
@@ -2,13 +2,16 @@ package es.brouse.game.panels;
 
 import es.brouse.game.objects.builders.ButtonBuilder;
 import es.brouse.game.objects.builders.LabelBuilder;
+import es.brouse.game.objects.builders.TextFieldBuilder;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class StartGamePanel extends Panel {
-
+public class StartGamePanel extends Panel implements ActionListener {
+    private TextFieldBuilder[] builders;
     @Override
     public void setUp(final JPanel panel) {
         panel.setLayout(new GridLayout(4, 2, 5, 5));
@@ -17,21 +20,40 @@ public class StartGamePanel extends Panel {
     @Override
     public void initComponents(final JPanel panel) {
         String[] names = {"USUARIO", "FILAS", "COLUMNAS"};
+        builders = new TextFieldBuilder[3];
 
+        int index = 0;
         for (String name : names) {
             panel.add(new LabelBuilder(name)
                     .setSize(new Dimension(50, 10))
                     .setBorder(new EmptyBorder(5, 5, 5, 5))
                     .getComponent());
-            panel.add(new JTextField());
+
+            builders[index] = new TextFieldBuilder();
+            panel.add(builders[index].getComponent());
+
+            index++;
         }
 
-        //Dummy content
+        //Add null content
         panel.add(new JLabel());
 
-        JComponent component = new ButtonBuilder("INICIAR PARTIDA", null, e -> {})
+        JComponent component = new ButtonBuilder("INICIAR PARTIDA", null, this)
                 .getComponent();
         component.setBorder(new EmptyBorder(0, 0, 5, 5));
         panel.add(component);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(((JButton) e.getSource()).getText());
+
+        JTextField username = ((JTextField) builders[0].getComponent());
+        JTextField rows = ((JTextField) builders[1].getComponent());
+        JTextField cols = ((JTextField) builders[2].getComponent());
+
+        String usernameVal = username.getText();
+        int rowsVals = Integer.parseInt(rows.getText());
+        int colsVal = Integer.parseInt(cols.getText());
     }
 }
