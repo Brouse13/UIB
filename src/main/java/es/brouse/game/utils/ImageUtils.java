3df95b5a47e99fe4bcd6c419489ceee01550f00c
Brouse_13 <unaiunai2004@gmail.com>
2023-05-10 12:09:57 +0200

    [NEW] Add rescale image util

--- src/main/java/es/brouse/game/utils/ImageUtils.java
+++ src/main/java/es/brouse/game/utils/ImageUtils.java
@@ -48,4 +48,19 @@ public class ImageUtils {
 
         return ImageIO.read(resource);
     }
+
+    public BufferedImage rescaleImage(BufferedImage image, int newWidth, int newLength) {
+        Image scaled = image.getScaledInstance(newWidth, newLength, Image.SCALE_SMOOTH);
+
+        //Create a buffered image with transparency
+        BufferedImage bufferedImage = new BufferedImage(newWidth, newLength, BufferedImage.TYPE_INT_ARGB);
+
+        //Draw the image on to the buffered image
+        Graphics2D bGr = bufferedImage.createGraphics();
+        bGr.drawImage(scaled, 0, 0, null);
+        bGr.dispose();
+
+        //Return the buffered image
+        return bufferedImage;
+    }
 }
