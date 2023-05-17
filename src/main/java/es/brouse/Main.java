208cec7a49749ea3afc2e45b6a89f3cbce4cba5c
Brouse_13 <unaiunai2004@gmail.com>
2023-05-17 11:35:06 +0200

    [NEW] Added logger to main

--- src/main/java/es/brouse/Main.java
+++ src/main/java/es/brouse/Main.java
@@ -3,13 +3,15 @@ package es.brouse;
 import es.brouse.screens.GameScreen;
 
 import java.awt.*;
+import java.util.logging.Logger;
 
 public class Main {
+    public static final Logger logger = Logger.getLogger(Main.class.getName());
     public static void main(String[] args) {
         new Main().start();
     }
 
     private void start() {
-        EventQueue.invokeLater(() ->new GameScreen().start());
+        EventQueue.invokeLater(() -> new GameScreen().start());
     }
 }
