29ee6bca00a59382ad42c8b187253374922339bc
Brouse_13 <unaiunai2004@gmail.com>
2023-06-04 00:50:20 +0200

    [NEW] Refactor class

--- src/main/java/es/brouse/game/panels/game/GameImagePanel.java
+++ src/main/java/es/brouse/game/panels/game/GameImagePanel.java
@@ -6,22 +6,16 @@ import es.brouse.game.objects.builders.ImageBuilder;
 import es.brouse.game.panels.Panel;
 import es.brouse.game.panels.iddle.IdlePanel;
 import es.brouse.game.utils.GameStats;
-import es.brouse.game.utils.ImageUtils;
 import es.brouse.game.utils.Ticker;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class GameImagePanel extends JPanel implements Panel, GameController.View {
     private final Dimension size;
-    private final List<SplitImage> subImages;
-    private final GameImageListener listener;
     private final GameController controller;
-    private final BufferedImage finalimage;
 
     /**
      * Main class constructor used to create new {@link GameImagePanel}
@@ -35,13 +29,11 @@ public class GameImagePanel extends JPanel implements Panel, GameController.View
         this.controller = new GameController(this, ticker, rows * cols);
 
         this.size = new Dimension(rows, cols);
-        this.finalimage = rescaleImage(image);
-        this.subImages = shuffleImages(finalimage, rows, cols);
-
-        listener = new GameImageListener(controller, subImages);
 
         setUp();
         initComponents();
+
+        controller.startGame(image, rows, cols);
     }
 
     @Override
@@ -51,7 +43,14 @@ public class GameImagePanel extends JPanel implements Panel, GameController.View
 
     @Override
     public void initComponents() {
-        for (SplitImage subImage : subImages) {
+        //This panel doesn't init any component on start
+    }
+
+    @Override
+    public void start(List<SplitImage> images) {
+        final GameImageListener listener = new GameImageListener(controller, images);
+
+        for (SplitImage subImage : images) {
             ImageBuilder imageBuilder = new ImageBuilder(subImage.getImage())
                     .setDimensions(new Dimension(-1, -1))
                     .setId(String.valueOf(subImage.getIndex()))
@@ -59,49 +58,6 @@ public class GameImagePanel extends JPanel implements Panel, GameController.View
 
             add(imageBuilder.getComponent());
         }
-    }
-
-    @Override
-    public JComponent getComponent() {
-        return this;
-    }
-
-    public GameController getController() {
-        return controller;
-    }
-
-    /**
-     * Rescale the given image.
-     *
-     * @param image image to rescale
-     * @return the rescaled image
-     */
-    private BufferedImage rescaleImage(BufferedImage image) {
-        Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
-
-        int width = (int) (size.getWidth() - 100);
-        int height = (int) (size.getHeight() - 150);
-
-        return new ImageUtils().rescaleImage(image, width, height);
-    }
-
-    /**
-     * Shuffle the given images.
-     *
-     * @param image image to shuffle
-     * @param rows rows of the shuffle
-     * @param cols cols of the shuffle
-     * @return the shuffled images
-     */
-    private List<SplitImage> shuffleImages(BufferedImage image, int rows, int cols) {
-        List<SplitImage> images = new ArrayList<>(new ImageUtils().split(image, rows, cols));
-        Collections.shuffle(images);
-        return images;
-    }
-
-    @Override
-    public void start(List<SplitImage> images) {
-
     }
 
     @Override
@@ -120,12 +76,21 @@ public class GameImagePanel extends JPanel implements Panel, GameController.View
     }
 
     @Override
-    public void renderEndGame(GameStats stats) {
+    public void renderEndGame(GameStats stats, BufferedImage image) {
         String message = stats.isWin() ?
                 "Enhorabuena, has conseguido solucionar el panel y ahs conseguido " + stats.getPoints() + " puntos" :
                 "No lo has conseguido, el tiempo se ha acabado";
 
         JOptionPane.showMessageDialog(IdlePanel.getInstance(), message);
-        IdlePanel.getInstance().getController().renderSolution(finalimage);
+        IdlePanel.getInstance().getController().renderSolution(image);
+    }
+
+    @Override
+    public JComponent getComponent() {
+        return this;
+    }
+
+    public GameController getController() {
+        return controller;
     }
 }
