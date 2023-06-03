a40e2ee8dfdffa4a35464163efa28346709ef19d
Brouse_13 <unaiunai2004@gmail.com>
2023-06-03 12:33:09 +0200

    [FIX] Removed unused statements

--- src/main/java/es/brouse/game/Game.java
+++ src/main/java/es/brouse/game/Game.java
@@ -2,10 +2,9 @@ package es.brouse.game;
 
 import es.brouse.game.screen.GameScreen;
 
-import javax.swing.*;
 import java.util.logging.Logger;
 
-public class Game extends JFrame {
+public class Game {
     public static final Logger logger = Logger.getLogger(Game.class.getSimpleName());
 
     public static void main(String[] args) throws InterruptedException {
