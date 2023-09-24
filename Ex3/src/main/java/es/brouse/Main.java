e20f6af14f6d0b22d01b1757a6c3eb43c3fbbb6c
Brouse_13 <unaiunai2004@gmail.com>
2023-09-24 18:42:55 +0200

    [FIX] Fixed javadocs

--- Ex3/src/main/java/es/brouse/Main.java
+++ Ex3/src/main/java/es/brouse/Main.java
@@ -10,11 +10,16 @@ public class Main {
     private static final List<Class<? extends Shape>> shapes = Arrays.asList(
             Triangle.class, Rectangle.class, Square.class, Circle.class);
 
+    /**
+     * Main class of the program
+     *
+     * @param args JVM args
+     */
     public static void main(String[] args) {
         final Main main = new Main();
 
         //We get the random shapes and an ordered version
-        List<Shape> shapes = main.shapeUtils.generateRandom(10);
+        List<Shape> shapes = main.shapeUtils.generateRandom(10_000);
         Map<Class<? extends Shape>, List<Shape>> classListMap = main.shapeUtils.filterByShape(shapes);
 
         //We print general info
@@ -33,16 +38,32 @@ public class Main {
         main.printTop10(shapes, Comparator.comparingDouble(Shape::getArea));
     }
 
+    /**
+     * Print the max area of the shapes
+     *
+     * @param shapes shapes to print
+     */
     private void maxArea(final List<Shape> shapes) {
         System.out.printf("Sum area: %,.2fm\n",
                 shapes.stream().mapToDouble(Shape::getArea).sum());
     }
 
+    /**
+     * Print the max perimeter of the shapes
+     *
+     * @param shapes shapes to print
+     */
     private void maxPerimeter(final List<Shape> shapes) {
         System.out.printf("Sum perimeter: %,.2fm\n",
                 shapes.stream().mapToDouble(Shape::getPerimeter).sum());
     }
 
+    /**
+     * Print the information of a specific shape.
+     *
+     * @param clazz class clazz of the shape
+     * @param shapes shapes to list
+     */
     private void printShapeInfo(final Class<?> clazz, final List<Shape> shapes) {
         if (shapes.isEmpty()) return;
 
@@ -61,7 +82,12 @@ public class Main {
                     """, clazz.getSimpleName(), sumPerimeter, maxPerimeter, sumArea, maxArea);
     }
 
-
+    /**
+     * Print the top 10 shapes from a collection.
+     *
+     * @param shapes shapes to print
+     * @param comparator comparator used to sort
+     */
     private void printTop10(List<Shape> shapes, Comparator<Shape> comparator) {
         shapes.sort(comparator);
 
